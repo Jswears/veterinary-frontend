f4a057d0ae42d5971553c4cc57781a70a4c98828
nicolapasa <nicola.pasa@gmail.com>
2023-08-05 12:59:10 +0200

    add icons

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFolderOpen, faCat, faPaw, faFolderPlus,  } from '@fortawesome/free-solid-svg-icons'
 
 const HomePage = () => {
 
@@ -7,21 +9,21 @@ const HomePage = () => {
       <div className="content-lg">
               <div className="button">
               
-              <Link to="/new-form">  <i className="fa-solid fa-folder-plus"></i> New Form</Link>
+              <Link to="/new-form">  <FontAwesomeIcon icon={faFolderPlus} />  New Form</Link>
 
                  </div>
  
                  <div className="button bg">
      
-                 <Link to="/your-forms">  <i className="fa-solid fa-folder-plus"></i> Your Form</Link>
+                 <Link to="/your-forms">  <FontAwesomeIcon icon={faFolderOpen} /> Your Form</Link>
                   </div>    
                   <div className="button bg1">
-                  <Link to="/new-pet">  <i className="fa-solid fa-folder-plus"></i> New pet</Link>
+                  <Link to="/new-pet">  <FontAwesomeIcon icon={faPaw} /> New pet</Link>
 
                   </div>  
                   <div className="button bg2">
                   
-                  <Link to={"/your-pets"}>  <i className="fa-solid fa-folder-plus"></i> Your pets</Link>
+                  <Link to={"/your-pets"}>  <FontAwesomeIcon icon={faCat} />  Your pets</Link>
                   </div> 
     </div>
      
