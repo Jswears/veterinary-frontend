8f82ae93bdfbd1f0543ceb1df055e16d1ebbdb4c
Jswears <ji.swearssalinas@gmail.com>
2023-08-08 20:17:18 +0200

    handling login/signup errormessage

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -4,7 +4,6 @@ import { AuthContext } from "../context/auth.context";
 import axios from "axios";
 import env from "../config";
 
-
 const SignupPage = () => {
   const [fullname, setFullname] = useState("");
   const [email, setEmail] = useState("");
@@ -21,11 +20,23 @@ const SignupPage = () => {
     e.preventDefault();
     try {
       const requestBody = { fullname, email, password };
-      const response = await axios.post(`${env.URL_BASE}/auth/signup`, requestBody);
-      console.log(response);
-      navigate("/login");
+      const response = await axios.post(
+        `${env.URL_BASE}/auth/signup`,
+        requestBody
+      );
+
+      if (response.status === 201) {
+        navigate("/login");
+      } else {
+        setErrorMessage(response.data.message);
+      }
     } catch (error) {
-      console.log(error);
+      if (error.response && error.response.data) {
+        setErrorMessage(error.response.data.message);
+      } else {
+        console.log(error);
+        setErrorMessage("Error");
+      }
     }
   };
 
@@ -58,6 +69,9 @@ const SignupPage = () => {
             />
             <button type="submit">Sign up!</button>
           </form>
+          <div className="error-message">
+            <p>{errorMessage && errorMessage}</p>
+          </div>
           <p>
             Already registered? <Link to={"/login"}>Log in</Link>
           </p>
