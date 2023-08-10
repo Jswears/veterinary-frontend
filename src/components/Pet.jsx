2a8418ec8b9912b8df89d25c388fa6f20d06ad80
Jswears <ji.swearssalinas@gmail.com>
2023-08-10 15:36:51 +0200

    default pet image

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -1,22 +1,24 @@
-import { Link } from "react-router-dom"
-import {  useContext } from "react";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
+import NoPetImage from "../assets/images/nopetimg.png";
 
-export const Pet = ({pet}) => {
- const {user}=useContext(AuthContext)   
+export const Pet = ({ pet }) => {
+  const { user } = useContext(AuthContext);
   return (
     <div className="pet-card" key={pet._id}>
-    <img src={pet.image} />
-    <h3>{pet.name}</h3>
-    {
-        user.role ==='admin' &&     
+      <img src={pet.image ? pet.image : NoPetImage} />
+      <h3>{pet.name}</h3>
+      {user.role === "admin" && (
         <>
-        <h3>Owner: {pet.customerId && pet.customerId.fullname} </h3>
-        <p>Age: {pet.age}</p>
-        <p>Specie: {pet.specie}</p>
+          <h3>Owner: {pet.customerId && pet.customerId.fullname} </h3>
+          <p>Age: {pet.age}</p>
+          <p>Specie: {pet.specie}</p>
         </>
-    }
-    { user.role!='admin' &&   <Link to={`/detailPet/${pet._id}`}>Details</Link>}
-  </div>
-  )
-}
+      )}
+      {user.role != "admin" && (
+        <Link to={`/detailPet/${pet._id}`}>Details</Link>
+      )}
+    </div>
+  );
+};
